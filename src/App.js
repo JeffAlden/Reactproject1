c0f0060efefe373aa610584962b6a5f83baac882
JeffAlden <134986403+JeffAlden@users.noreply.github.com>
2023-07-26 21:12:20 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes instead of Switch
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Home, About, Contact, Blog } from './components/Pages';
